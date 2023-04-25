a64fd9573849c54a62be0f59f4581059443c2200
Patrik Aradi <aradipatrik@gmail.com>
2023-04-25 11:20:05 +0800

    add module handlebars files

--- src/helpers.rs
+++ src/helpers.rs
@@ -26,3 +26,29 @@ pub fn to_pascal(
     out.write(param.value().render().to_string().to_case(Case::Pascal).as_ref())?;
     Ok(())
 }
+
+pub fn to_camel(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext,
+    out: &mut dyn Output,
+) -> HelperResult {
+    let param = h.param(0).unwrap();
+
+    out.write(param.value().render().to_string().to_case(Case::Camel).as_ref())?;
+    Ok(())
+}
+
+pub fn to_kebab(
+    h: &Helper,
+    _: &Handlebars,
+    _: &Context,
+    _: &mut RenderContext,
+    out: &mut dyn Output,
+) -> HelperResult {
+    let param = h.param(0).unwrap();
+
+    out.write(param.value().render().to_string().to_case(Case::Kebab).as_ref())?;
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -9,24 +9,28 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let base_package = &args[1];
-    let first_page = &args[2];
-    let name = &args[3];
+    let first_page = &args[3];
+    let module_name = &args[2];
+    let app_name = &args[4];
 
     let mut handlebars = Handlebars::new();
     register_helpers(&mut handlebars);
 
-    let source = include_str!("FeatureComponent.handlebars");
+    let source = include_str!("templates/api/FeatureEntry.handlebars");
     assert!(handlebars.register_template_string("feature_component", source).is_ok());
 
     let mut data = BTreeMap::new();
-    data.insert("name".to_string(), name);
+    data.insert("module".to_string(), module_name);
     data.insert("base_package".to_string(), base_package);
-    data.insert("first_page_name".to_string(), first_page);
+    data.insert("first_page".to_string(), first_page);
+    data.insert("app".to_string(), app_name);
 
     println!("{}", handlebars.render("feature_component", &data).unwrap())
 }
 
 fn register_helpers(handlebars: &mut Handlebars) {
-    handlebars.register_helper("to_flat", Box::new(helpers::to_flat));
-    handlebars.register_helper("to_pascal", Box::new(helpers::to_pascal));
+    handlebars.register_helper("flat", Box::new(helpers::to_flat));
+    handlebars.register_helper("pascal", Box::new(helpers::to_pascal));
+    handlebars.register_helper("camel", Box::new(helpers::to_camel));
+    handlebars.register_helper("kebab", Box::new(helpers::to_kebab));
 }
